3263df2003d2a88fce1f77d7b579c26ba79e18b6
MR-LEO <73919659+wonkru-bot@users.noreply.github.com>
2024-03-23 12:05:55 +0530

    Small update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://192.168.1.5:5000"
+    baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: "http://192.168.1.5:5000",
+    baseURL: process.env.REACT_APP_SERVER_URL,
     headers: {
         'Content-Type': 'application/json',
     },
