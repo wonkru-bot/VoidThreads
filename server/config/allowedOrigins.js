d638a047f77bff2b5c1e919c9e2cc78c885ed64a
Manu <manukrishnaos45@gmail.com>
2024-03-24 20:31:06 +0530

    client-port: 3000

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const allowedOrigins = [
-    'http://192.168.1.5:3001',
-    'http://localhost:3001',
+    'http://192.168.1.5:3000',
+    'http://localhost:3000',
 ];
 
 // process.env.CLIENT_URL,
-// 'http://192.168.1.5:3001',
-// 'http://localhost:3001',
+// 'http://192.168.1.5:3000',
+// 'http://localhost:3000',
 // 'http://localhost:5001',
 // 'http://192.168.1.4:5001',
 
